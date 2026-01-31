658acecf6b577ce174274f5b15ce5eb9257aec14
Varun Kushwah <varun.kush3@gmail.com>
2026-01-31 23:14:07 +0530

    Amendment in Bcrypt Encoder

--- src/main/java/com/practice/springSecurityPractice/config/ProjectConfig.java
+++ src/main/java/com/practice/springSecurityPractice/config/ProjectConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,33 +16,31 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class ProjectConfig {
 
     @Bean
-    public UserDetailsService userDetailsService(){
-        UserDetails user = User.withUsername("John")
-                .password("{noop}12345")
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("Jane")
+                .password(passwordEncoder().encode("12345"))
                 .authorities("read")
                 .build();
         return new InMemoryUserDetailsManager(user);
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public SecurityFilterChain filterChain(@NonNull HttpSecurity http) throws Exception{
-        http.httpBasic(Customizer.withDefaults());
-        http.cors(AbstractHttpConfigurer::disable);
-        http.authorizeHttpRequests(c -> c.anyRequest().authenticated());
-        UserDetails user = User.withUsername("Jane")
-                .password("{noop}12345")
-                .authorities("read")
-                .build();
-        UserDetailsService userDetailsService = new InMemoryUserDetailsManager(user);
-        http.userDetailsService(userDetailsService);
+    public SecurityFilterChain filterChain(@NonNull HttpSecurity http) throws Exception {
+        http
+                .httpBasic(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(c -> c.anyRequest().authenticated());
+
         return http.build();
     }
+
 }
